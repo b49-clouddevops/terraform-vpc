0d7819590398eb4238ebd37d7f01f50fbb795d43
mvpc-11232 <mvp2612@hotmail.com>
2022-08-26 08:44:00 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 # }
 
 module "vpc" {
-  source = "git::https://github.com/b49-clouddevops/tf-module-vpc.git"
+  source = "./vendor/modules/vpc"
 }
 
 # You cannot parameterise the source section of the terraform module : Branc name cannot be parameterised. 
