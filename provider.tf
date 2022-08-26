5af4af1fef970862b3e3d1d8d6766cd4ca5edfd6
mvpc-11232 <mvp2612@hotmail.com>
2022-08-26 08:40:11 +0530

    Auto Push Commit

--- provider.tf
+++ provider.tf
@@ -4,17 +4,7 @@ provider "aws" {
 
 
 terraform {
-  backend "s3" {
-    bucket = "b49-rf-remote-state-bucket"
-    key    = "dev/vpc/terraform.tfstate"
-    region = "us-east-1"
-  }
+  backend "s3" { }
 }
 
-terraform {
-  backend "s3" {
-    bucket = "b49-rf-remote-state-bucket"
-    key    = "dev/vpc/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# Backend bucket will be mentioned in the respective environment folder.
