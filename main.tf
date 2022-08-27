cbfb92c6130bda8e2b502b4405528e133de733ec
mvpc-11232 <mvp2612@hotmail.com>
2022-08-27 08:13:11 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ module "vpc" {
   VPC_CIDR     = var.VPC_CIDR
   ENV          = var.ENV
   SUBNET_CIDR  = var.SUBNET_CIDR 
+  AZ           = var.AZ
 }
 
 # You cannot parameterise the source section of the terraform module : Branc name cannot be parameterised. 
