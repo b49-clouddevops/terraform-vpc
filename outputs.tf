d75bf2bbb1c749399c23c54df8c717a95402cfc8
mvpc-11232 <mvp2612@hotmail.com>
2022-09-08 18:11:06 +0530

    Auto Push Commit

--- outputs.tf
+++ outputs.tf
@@ -8,11 +8,11 @@ output "VPC_CIDR" {
 }
 
 output "PRIVATE_SUBNET_IDS" {
-    value = module.vpc.PUBLIC_SUBNET_IDS
+    value = module.vpc.PRIVATE_SUBNET_IDS
 }
 
 output "PUBLIC_SUBNET_ID" {
-    value = module.vpc.PRIVATE_SUBNET_IDS
+    value = module.vpc.PUBLIC_SUBNET_IDS
 }
 
 output "PRIVATE_SUBNET_CIDR" {
